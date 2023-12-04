f9b0b9ab99e243e33fe78a1a840b772c77402c54
Muaz Khan <muazmkhan107@gmail.com>
2023-12-04 21:57:36 +0500

    Switching scenes

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,10 +1,22 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
+    Stage stage;
     @FXML
-    public void signup(ActionEvent e){
-        System.out.println("Sign up");
+    public void signup(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Signup.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
     public void login(ActionEvent e){
         System.out.println("Log in");
---
+++ src/main/java/SignUpController.java
@@ -0,0 +1,21 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SignUpController {
+    Stage stage;
+    @FXML
+    public void goHome(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
